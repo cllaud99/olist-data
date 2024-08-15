3e4748453c377fed83d096d5016a773b287cb39c
Claudio Pontes <claudiohpontes@gmail.com>
2024-08-14 23:10:27 -0300

    Adicionado extração do kaggle

---
+++ src/extract.py
@@ -0,0 +1,39 @@
+import os
+from dotenv import load_dotenv
+import zipfile
+from logger_config import logger  # Importa a configuração do logger
+
+# Carrega as variáveis de ambiente do arquivo .env
+load_dotenv()
+
+# Configura as variáveis de ambiente necessárias para o Kaggle
+os.environ['KAGGLE_USERNAME'] = os.getenv('KAGGLE_USERNAME')
+os.environ['KAGGLE_KEY'] = os.getenv('KAGGLE_KEY')
+
+# Define o diretório de destino
+destination_dir = "data/landing_zone"
+
+# Cria a pasta caso ela não exista
+os.makedirs(destination_dir, exist_ok=True)
+
+try:
+    # Baixa o dataset
+    logger.info("Baixando o dataset do Kaggle...")
+    os.system(f'kaggle datasets download -d olistbr/brazilian-ecommerce -p {destination_dir}')
+
+    # Caminho do arquivo ZIP baixado
+    zip_file_path = os.path.join(destination_dir, "brazilian-ecommerce.zip")
+
+    # Extrai o arquivo .zip
+    logger.info("Extraindo o arquivo ZIP...")
+    with zipfile.ZipFile(zip_file_path, 'r') as zip_ref:
+        zip_ref.extractall(destination_dir)
+
+    # Remove o arquivo ZIP após a extração
+    logger.info("Removendo o arquivo ZIP...")
+    os.remove(zip_file_path)
+
+    logger.success("Download, extração e limpeza concluídos com sucesso!")
+
+except Exception as e:
+    logger.error(f"Ocorreu um erro: {e}")
